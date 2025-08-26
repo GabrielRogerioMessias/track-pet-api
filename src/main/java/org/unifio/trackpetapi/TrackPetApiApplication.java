0edf508bac815ead15bcd3f4a5c7214893e0e8f6
Gabriel Messias <gamessiasjt@gmail.com>
2025-08-25 22:09:25 -0300

    feat: criando projeto backend

---
+++ src/main/java/org/unifio/trackpetapi/TrackPetApiApplication.java
@@ -0,0 +1,13 @@
+package org.unifio.trackpetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackPetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unifio/trackpetapi/TrackPetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unifio.trackpetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackPetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
